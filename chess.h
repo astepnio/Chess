c4647e9e4477ec1431ac2bd53533e20d30c6a36e
astepnio <aidan.stepniowski@gmail.com>
2023-03-23 14:43:39 -0600

    Create chess.h

---
+++ chess.h
@@ -0,0 +1,11 @@
+#pragma once //source
+
+#define NOMINMAX
+
+#include <iostream>
+#include "Windows.h"
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <random>
+#include <limits>
